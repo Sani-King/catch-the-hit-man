149cfbd1223b2f0748cd4995691b706533aba00e
Sani-King <101492948+Sani-King@users.noreply.github.com>
2022-04-01 10:37:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/*function validate(){const check10= document.getElementById('duck10');
+   if(duck10.ckecked == 1) {
+      alert('Victory');
+   }
+}*/
+
+
+function validate() {
+   try {
+      outputScreen.value = eval(outputScreen.value)
+
+   } catch (err) {
+      alert('OP Gameplay Your Prize Is Girlfriend Contact Us Mobile Number +8801760631709 ');
+   }
+}
